b4f4bb663971ba075e5cf8af60a248518144c45b
21D4N404 <42216966+21D4N404@users.noreply.github.com>
2018-09-09 16:06:18 +0700

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,17 @@
+green="\033[32;1m"
+yellow="\033[33;1m"
+indigo="\033[34;1m"
+red="\033[35;1m"
+purple="\033[37;1m"
+cyan="\033[36;1m"
+white="\033[39;1m"
+echo $red
+pkg install toilet -y
+clear
+echo $green
+pkg install figlet -y
+clear
+echo $cyan
+gem install toilet -y
+clear
+#WJIB install.sh TERLEBIH DAHULU SEBELUM MENJALANKAN TOOLS INI
