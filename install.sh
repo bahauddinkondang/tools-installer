9c0b93851804f024fd898f19d5f399ca503581d8
21D4N404 <42216966+21D4N404@users.noreply.github.com>
2018-09-10 23:24:19 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,18 +1,21 @@
 green="\033[32;1m"
 yellow="\033[33;1m"
 indigo="\033[34;1m"
-red="\033[35;1m"
-white="\033[37;1m"
+ungu="\033[35;1m"
 cyan="\033[36;1m"
+white="\033[37;1m"
+red="\033[31;1m"
 
 sleep 1
-echo $red"
- _
-/ |
-| |
-| |
-|_|
-"
+echo '''\a
+\033[31;1m 	_
+\033[31;1m / |
+\033[31;1m	| |
+\033[31;1m | |
+\033[31;1m |_| '''
+
+
+echo
 echo $red
 pkg install toilet -y
 clear
